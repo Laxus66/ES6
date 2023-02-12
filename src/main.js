1839c7755b478e8645cee315f00c985a7e5b482c
Laxus66 <Laxus2301@gmail.com>
2023-02-13 03:28:58 +0700

    Install Bootstrap and Tailwind CSS

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-console.log("Hello Laxus");
-console.log("Hello Laxus 02");
+import css from "./main.module.css"
+import "bootstrap/dist/js/bootstrap.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+document.querySelector("#app").innerHTML =
+    `
+    <div class="${css.red}">
+        Hello Laxus
+    </div>
+    `
